925bdc469e59ac2bfcdf33fc2f8d8a6266358217
Erick Gómez <erick.gomez@gruporaosari.mx>
2026-03-02 15:53:24 -0600

    pagination bug fix and admin pagination and search

--- controllers/admin/admin.controllers.js
+++ controllers/admin/admin.controllers.js
@@ -2,10 +2,20 @@ const Job = require("../../models/jobModel")
 const Modality = require("../../models/modalityModel")
 const TypeOfJob = require("../../models/typeOfJobModel")
 const Company = require("../../models/companyModel")
+const { Op } = require("sequelize");
 
 exports.getAllJobForAdminPanelController = async (req, res) => {
     try {
-        const jobs = await Job.findAll({
+        const { page, limit, search } = req.query;
+        const parsedPage = Math.max(1, parseInt(page), 1);
+        const parsedLimit = Math.max(1, parseInt(limit), 20);
+        const offset = (parsedPage - 1) * parsedLimit;
+        const { rows, count } = await Job.findAndCountAll({
+            where: {
+                title: {
+                    [Op.like]: `%${search}%`
+                }
+            },
             attributes: ["id", "title", "location", "salary_min", "salary_max", "isApproved", "createdAt"],
             include: [
                 {
@@ -23,13 +33,16 @@ exports.getAllJobForAdminPanelController = async (req, res) => {
             ],
             order: [
                 ["createdAt", "DESC"]
-            ]
+            ],
+            limit: parsedLimit,
+            offset: offset
         });
         res.status(200).json({
             ok: true,
             code: "SUCCESS",
             message: "Jobs fetched successfully",
-            jobs: jobs
+            jobs: rows,
+            count: count
         });
     } catch (e) {
         console.error(e);
@@ -43,17 +56,29 @@ exports.getAllJobForAdminPanelController = async (req, res) => {
 
 exports.getAllCompaniesForAdminPanelController = async (req, res) => {
     try {
-        const companies = await Company.findAll({
+        const { page, limit, search } = req.query;
+        const parsedPage = Math.max(1, parseInt(page), 1);
+        const parsedLimit = Math.max(1, parseInt(limit), 20);
+        const offset = (parsedPage - 1) * parsedLimit;
+        const { rows, count } = await Company.findAndCountAll({
+            where: {
+                name: {
+                    [Op.like]: `%${search}%`
+                }
+            },
             attributes: ["id", "name", "country", "state", "city", "logo", "createdAt", "isApproved"],
             order: [
                 ["createdAt", "DESC"]
-            ]
+            ],
+            limit: parsedLimit,
+            offset: offset
         });
         res.status(200).json({
             ok: true,
             code: "SUCCESS",
             message: "Companies fetched successfully",
-            companies: companies
+            companies: rows,
+            count: count
         });
     } catch (e) {
         console.error(e);
